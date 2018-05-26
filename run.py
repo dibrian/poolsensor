bcf7056e8f4fe00498fbf7527b87b326e6521fa7
gergoke <gergely.csuhai@gmail.com>
2018-05-26 17:20:10 +0200

    New function: Init phase

--- run.py
+++ run.py
@@ -6,6 +6,73 @@ import ReleKapcs, GetTemp, log, time, datetime
 from model import Sensor
 from model import Relay
 
+
+def Phase_Init():
+    global medence, avg_temp, count, relays
+    log.prnt(TS.most(1) + "---------------Rendszerinditas------------------")
+    # Init sensors
+    log.prnt(TS.most(1) + ". Szenzorok inicializalasa...")
+    global sensors
+    =[]
+    medence = Sensor()
+    medence.id = '28-0417c2d68cff'
+    medence.name = "Medence (1)"
+    medence.temp = GetTemp.gettemp(medence.id);
+    # sensors.append(medence)
+    sensor1 = Sensor()
+    sensor1.id = '28-0417c3b21dff'
+    sensor1.name = "Panel (1)"
+    #  sensor.temp = GetTemp.gettemp(sensor.id);
+    sensor1.temp = "0"
+    sensors.append(sensor1)
+    sensor2 = Sensor()
+    sensor2.id = '28-0417c3b93dff'
+    sensor2.name = "Panel (2)"
+    #    sensor.temp = GetTemp.gettemp(sensor.id);
+    sensor2.temp = "0"
+    sensors.append(sensor2)
+    sensor3 = Sensor()
+    sensor3.id = '28-0417c3b7a4ff'
+    sensor3.name = "Panel (3)"
+    #  sensor.temp = GetTemp.gettemp(sensor.id)
+    sensor3.temp = "0"
+    sensors.append(sensor3)
+    avg_temp = 0
+    count = 0
+    while count < len(sensors):
+        sensors[count].temp = GetTemp.gettemp(sensors[count].id)
+        log.prnt(TS.most(1) + ".. " + sensors[count].name + ", homersekleti erteke: " + "{:.3f}".format(
+            sensors[count].temp / float(1000)))
+        avg_temp = avg_temp + sensors[count].temp
+        count += 1
+    avg_temp = (avg_temp / (count))
+    log.prnt(TS.most(1) + ".. Panelek atlaghomerseklete: %s" % (format(avg_temp / float(1000))))
+    #  log.prnt(TS.most(1) + "..")
+    # Init relays
+    log.prnt(TS.most(1) + ". Relek inicializalasa...")
+    relays = []
+    relay1 = Relay()
+    relay1.id = 17
+    relay1.name = "Melegito szivattyu"
+    relay1.state = ReleKapcs.mizu(relay1.id)
+    relays.append(relay1)
+    relay2 = Relay()
+    relay2.id = 18
+    relay2.name = "Tisztito szivattyu"
+    relay2.state = ReleKapcs.mizu(relay2.id)
+    relays.append(relay2)
+    count = 0
+    while count < len(relays):
+        if relays[count].state == False:
+            log.prnt(TS.most(1) + ".. " + relays[count].name + ", allapota: <<< On >>>")
+        else:
+            log.prnt(TS.most(1) + ".. " + relays[count].name + ", allapota: <<< Off >>>")
+        count += 1
+    log.prnt(TS.most(1) + ". Inditasi ido: %s ora" % (str(time_start)))
+    log.prnt(TS.most(1) + ". Kikapcsolasi ido: %s ora" % (str(time_stop)))
+    log.prnt(TS.most(1) + "--------------Sikeres rendszerinditas-------------")
+
+
 if __name__ == '__main__':
 
 
@@ -18,87 +85,8 @@ if __name__ == '__main__':
 
 
   # Script has been called directly
-  
-  log.prnt(TS.most(1) + "---------------Rendszerinditas------------------")
-  
-  #Init sensors
-  
-  log.prnt(TS.most(1) + ". Szenzorok inicializalasa...")
-  
-  global sensors=[]
 
-  medence = Sensor()
-  medence.id = '28-0417c2d68cff'
-  medence.name = "Medence (1)"
-  medence.temp = GetTemp.gettemp(medence.id);
-   # sensors.append(medence)
-
-
-  sensor1 = Sensor()
-  sensor1.id = '28-0417c3b21dff'
-  sensor1.name = "Panel (1)"
-#  sensor.temp = GetTemp.gettemp(sensor.id);
-  sensor1.temp = "0"
-  sensors.append(sensor1)
-  
-  sensor2 = Sensor()
-  sensor2.id = '28-0417c3b93dff'
-  sensor2.name = "Panel (2)"
-#    sensor.temp = GetTemp.gettemp(sensor.id);
-  sensor2.temp = "0"
-  sensors.append(sensor2)
-  
-  sensor3 = Sensor()
-  sensor3.id = '28-0417c3b7a4ff'
-  sensor3.name = "Panel (3)"
-#  sensor.temp = GetTemp.gettemp(sensor.id)
-  sensor3.temp = "0"
-  sensors.append(sensor3)
-     
-  avg_temp = 0
-  
-  count = 0
-  while count < len(sensors):
-    sensors[count].temp = GetTemp.gettemp(sensors[count].id)
-    log.prnt(TS.most(1) + ".. " + sensors[count].name + ", homersekleti erteke: " + "{:.3f}".format(sensors[count].temp/float(1000)))
-    avg_temp = avg_temp + sensors[count].temp
-    count += 1
-  
-  avg_temp = (avg_temp / (count))
-  
-  log.prnt(TS.most(1) + ".. Panelek atlaghomerseklete: %s" %(format(avg_temp/float(1000))))
-#  log.prnt(TS.most(1) + "..")
-  
-  #Init relays
-  log.prnt(TS.most(1) + ". Relek inicializalasa...")
-  
-  relays = []
-  
-  relay1 = Relay()
-  relay1.id=17
-  relay1.name="Melegito szivattyu"
-  relay1.state=ReleKapcs.mizu(relay1.id)
-  relays.append(relay1)
-  
-  relay2 = Relay()
-  relay2.id=18
-  relay2.name="Tisztito szivattyu"
-  relay2.state=ReleKapcs.mizu(relay2.id)
-  relays.append(relay2)
-  
-  count = 0
-  while count < len(relays):
-    if relays[count].state == False:  
-      log.prnt(TS.most(1) + ".. " + relays[count].name + ", allapota: <<< On >>>")
-    else:
-      log.prnt(TS.most(1) + ".. " + relays[count].name + ", allapota: <<< Off >>>")
-    count += 1
-    
-  
-  log.prnt(TS.most(1) + ". Inditasi ido: %s ora" %(str(time_start)))
-  log.prnt(TS.most(1) + ". Kikapcsolasi ido: %s ora" %(str(time_stop)))
-  
-  log.prnt(TS.most(1) + "--------------Sikeres rendszerinditas-------------")
+  Phase_Init()
 #BU_logic
 
 def logValues():
